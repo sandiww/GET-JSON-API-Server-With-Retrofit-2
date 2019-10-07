b61a984500b321057c85d82d53b3dd185ab654f9
unknown <sandirpl3e@gmail.com>
2019-10-07 12:21:36 +0700

    Change colorAccent

--- app/src/main/java/com/codewith/sandisuryadi/getretrofit/util/api/UtilsApi.java
+++ app/src/main/java/com/codewith/sandisuryadi/getretrofit/util/api/UtilsApi.java
@@ -2,7 +2,7 @@ package com.codewith.sandisuryadi.getretrofit.util.api;
 
 public class UtilsApi {
 
-    // 10.0.2.2 ini adalah localhost.
+    // Ini adalah localhost.
     public static final String BASE_URL_API = "http://192.168.1.11/mahasiswa/index.php/";
 
     // Mendeklarasikan Interface BaseApiService
